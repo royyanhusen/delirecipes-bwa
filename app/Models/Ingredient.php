387cb933df4a21620ce11cf001e0454a83c514e1
royyan <royanhusen007@gmail.com>
2024-12-05 21:07:38 +0700

    model fillable dan relationship

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ingredient extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+    ];
+
+    
+    public function recipeIngredients(): HasMany
+    {
+        return $this->hasMany(recipeIngredient::class, 'recipe_id');
+    }
 }
--- app/Models/RecipePhoto.php
+++ app/Models/RecipePhoto.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RecipePhoto extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'photo',
+        'recipe_id',
+    ];
+
+    public function recipe():BelongsTo
+    {
+        return $this->belongsTo(Recipe::class, 'recipe_id');
+    }
 }
